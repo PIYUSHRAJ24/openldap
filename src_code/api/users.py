8f0570901defbd446ee289679818a392312f3a14
Subhash <kaashyan4u@gmail.com>
2024-09-03 15:20:40 +0530

    Active Users

--- src_code/api/users.py
+++ src_code/api/users.py
@@ -347,3 +347,63 @@ def default_remove():
         return res, 400
     
 
+@bp.route("/check_user_status", methods=["GET"])
+def check_user_status():
+    
+    form_data = request.form
+    org_id = form_data.get('org_id')
+    digilockerid = form_data.get('digilockerid')
+    is_active = form_data.get('is_active')
+     
+    if not org_id:
+        return {STATUS: ERROR, ERROR_DES: "Provide a valid org_id"}, 400
+    
+    try:
+               
+        if org_id:
+            # Retrieve all users (active and inactive) based on org_id
+            query = {'org_id': org_id}
+            
+            # If we want to get only active users
+            if is_active is not None:
+                query['is_active'] = is_active
+                
+        elif org_id and digilockerid:
+            # Retrieve the information for a specific user based on access_id and org_id
+            strtohash = org_id + digilockerid
+            access_id = hashlib.md5(strtohash.encode()).hexdigest()
+            query = {'access_id': access_id, 'org_id': org_id}
+        
+        res, status_code = MONGOLIB.org_eve(CONFIG["org_eve"]["collection_rules"], query, {}, limit=500)
+        response_status = res.get('status')
+
+        if response_status == 'success':
+            if status_code != 200:
+                return {STATUS: SUCCESS, RESPONSE: f"No matching records found for org_id {org_id}"}, 404
+                
+            else:
+                if res['response']:
+                    record = res['response'][0]
+                    is_active = record.get('is_active')
+                    
+                    log_data = {RESPONSE: [res]}
+                    logarray.update(log_data)
+                    RABBITMQLOGS.send_to_queue(logarray, 'Logstash_Xchange', 'org_logs_')
+                    return {
+                        STATUS: SUCCESS,
+                        RESPONSE: record
+                    }, 200
+                else:
+                    return {STATUS: SUCCESS, RESPONSE: f"No matching records found for org_id {org_id}"}, 404
+        else:
+            return {STATUS: ERROR, ERROR_DES: "Failed to fetch data"}, 500
+    
+    except Exception as e:
+        log_data = {RESPONSE: str(e)}
+        logarray.update(log_data)
+        RABBITMQLOGS.send_to_queue(logarray, 'Logstash_Xchange', 'org_logs_')
+        return {
+            STATUS: ERROR,
+            ERROR_DES: "Technical error",
+            RESPONSE: str(e)
+        }, 400
