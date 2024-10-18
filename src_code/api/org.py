009d17e405a1cf80f7157fa2dcfaaa4ec6e1a50e
Abhijeet Anand <abhijeet.a912@gmail.com>
2024-10-18 09:59:15 +0530

    msme activation case handled

--- src_code/api/org.py
+++ src_code/api/org.py
@@ -1850,13 +1850,14 @@ def pull_all_ids(data, org_id):
         REDISLIB.set('Debug_pull_all_ids_001', str(e), 3600)
         return {"status": "error", 'response': str(e)}
 
-def move_data_attempts_prod(org_id):
+def move_data_attempts_prod(org_id_req):
     try:
-        req = {'entity_partner_org_id': org_id}
+        req = {'$or':[{'entity_partner_org_id': org_id_req}, {'org_id':org_id_req}]}
         res, status_code = MONGOLIB.org_eve(CONFIG["org_eve"]["collection_attempts"], req, {})
         if status_code == 200:
             post_data_details = {}
             r = res[RESPONSE][0]    
+            org_id = r.get('org_id')
             transactionid = r.get('transactionid', '') 
             post_data_details['org_id'] = org_id
             post_data_details['is_approved'] = "YES"
