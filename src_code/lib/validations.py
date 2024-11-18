454b1f9d9657dc4a86f58ff948257bc652dbbaee
Bhavishya Kumar <bhavishya.choudhary@gmail.com>
2024-11-18 09:35:52 +0530

    update regex of gstin

--- src_code/lib/validations.py
+++ src_code/lib/validations.py
@@ -96,7 +96,8 @@ class Validations:
         return re.match(pattern, str(code))
     
     def is_valid_gstin(self, code):
-        pattern = r"^\d{2}[A-Z]{5}\d{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$"
+        pattern = r'^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[A-Z0-9]{1}[Z]{1}[A-Z0-9]{1}$'
+        # pattern = r"^\d{2}[A-Z]{5}\d{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$"
         return re.match(pattern, str(code))
     
     def is_valid_udyam(self, code):
