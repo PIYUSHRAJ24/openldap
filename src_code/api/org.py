1e93565a4a583f82377e6cd9e1e45104c7b8b882
Bhavishya Kumar <bhavishya.choudhary@gmail.com>
2024-12-05 16:53:44 +0530

    update code path

--- src_code/api/org.py
+++ src_code/api/org.py
@@ -1967,7 +1967,7 @@ def move_data_attempts_prod(org_id_req):
             update_appved = {"is_approved":"YES"}
             update_appved_res = MONGOLIB.org_eve_update(CONFIG["org_eve"]["collection_attempts"], update_appved,transactionid)   
             
-            path =  org_id + '_common'
+            path =  f"{org_id}_common"
             CLIENT_ID = CONFIG['org_drive_api']['client_id']
             ts = str(int(time.time()))
             plain_txt = f"{CONFIG.get("org_drive_api", "client_secret")}{CLIENT_ID}{ts}"
